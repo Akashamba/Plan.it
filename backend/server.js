40952e5d350c8a3da94777cd2677b00f2896320e
akashamba <akashambashankar@gmail.com>
2025-08-13 14:06:44 -0400

    using simplified  vercel-express-template

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,28 @@
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.json({
+//     message: "Hello from server!",
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`App listening on port ${port}`);
+// });
+
+// module.exports = app;
+
 const express = require("express");
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello from server!",
-  });
+// app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+  res.json({ message: "hello from server!" });
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
