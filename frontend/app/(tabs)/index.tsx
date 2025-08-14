a37f597863145073398da58bcbae1765bfec8a2e
akashamba <akashambashankar@gmail.com>
2025-08-13 23:24:13 -0400

    update error and loading of temp display

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -11,6 +11,9 @@ export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Text>{process.env.EXPO_PUBLIC_API_URL}</Text>
+      {loading && <Text style={styles.title}>Loading...</Text>}
+      {error && <Text style={styles.title}>Error: {error.message}</Text>}
       {!loading && !error && <Text style={styles.title}>{data?.message}</Text>}
       <View
         style={styles.separator}
