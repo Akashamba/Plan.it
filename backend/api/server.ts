d0f0426cca3da25c4bee7657d352b6f264d6d5e0
akashamba <akashambashankar@gmail.com>
2025-08-13 22:53:42 -0400

    move to ts and update vercel.json

---
+++ backend/api/server.ts
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.json({ message: "hello from server!" });
+});
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
