183d27e8e2ecd7c8b35d58aa319c4b27b44ba411
akashamba <akashambashankar@gmail.com>
2025-08-16 17:02:31 -0400

    add better-auth pt1: db set up complete

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./db/schema.ts",
+  schema: "./db/*schema.ts",
   dialect: "postgresql",
+  tablesFilter: `${process.env.TABLES_PREFIX}_*`,
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
